ebda4cbf4e960296375abd4f0df16517ba996681
Ayden Mactavish <inbox@macnco.one>
2021-06-28 23:44:05 +0300

    1:280621
    New language - Spanish by ﻿FusionDrake﻿
    New language - German by ﻿TOJU﻿
    New language - Turkish by ﻿Wolflix

---
+++ lua/msd/language/de.lua
@@ -0,0 +1,276 @@
+MSD.Language["de"] = {
+
+
+	-- UI
+
+
+	lang_name = "Deutsch",
+
+
+	ok = "OK",
+	map = "Karte",
+	off = "Aus",
+	on = "An",
+	time_add = "Zeit zum Hinzufügen",
+	type = "Typ",
+	delay = "Verzögerung",
+	cancel = "Abbrechen",
+	enable = "Aktivieren",
+	model = "Model",
+	name = "Name",
+	settings = "Einstellungen",
+	editor = "Editor",
+	red = "Rot",
+	green = "Grün",
+	blue = "Blau",
+	admin_menu = "Administrationsmenü",
+	ui_settings = "UI Einstellungen",
+	active = "Aktiv",
+	inactive = "Inaktiv",
+	disabled = "Deaktiviert",
+	warning = "Warnung!",
+	remove = "Entfernen",
+	theme = "Theme",
+	dark_theme = "Dunkles Theme",
+	payment = "Zahlung",
+	load_autosave = "Letzte automatische Speicherung laden?",
+	load_save = "Speicherung laden",
+	create_new = "Neu erstellen",
+	enable_option = "Option aktivieren",
+	main_opt = "Hauptoptionen",
+	copy_data = "Kopiere Daten",
+	save_chng = "Speichere Änderungen",
+	enter_name = "Gib den Namen ein",
+	enter_id = "Gib die ID ein",
+	confirm_action = "Bitte bestätige deine Aktion",
+	check_fpr_errors = "Auf Fehler prüfen",
+	enter_description = "Gib die Beschreibung ein",
+	cooldown_ok = "Abklingzeit bei Erfolg",
+	cooldown_fail = "Abklingzeit bei Fehlschlag",
+	s_team_whitelist = "Team-Whitelist einrichten",
+	whitelist_blacklist = "Die Whitelist als Blacklist benutzen",
+	custom_val = "Setze spezifischen Wert",
+	set_hp_full = "Setze volle HP",
+	dist_to_close = "Distanz zum nächsten",
+
+	e_text = "Text eingeben",
+	e_number = "Nummer eingeben",
+	e_class = "Klasse eingeben",
+	e_value = "Wert eingeben",
+	e_blank_dis = "Leer lassen, um es zu deaktiveren",
+	e_blank_default = "Leer lassen, um Standard zu benutzen",
+	e_url = "URL eingeben",
+	e_model = "Modelpfad eingeben",
+	e_material = "Materialpfad eingeben",
+	e_wep_class = "Waffenklasse eingeben",
+	e_ent_class = "Entityklasse eingeben",
+	e_veh_class = "Fahrzeugklasse eingeben",
+	e_npc_class = "NPC-Klasse eingeben",
+
+
+	select_ammo = "Ausgewählte Munition",
+	amount_ammo = "Munitionsmenge",
+	disable_phys = "Physik deaktivieren",
+	none = "Nichts",
+	custom_icon = "Setzte eigenes Icon",
+	weapon_name = "Waffenname",
+	moveup = "Aufwärts bewegen",
+	movedown = "Abwärts bewegen",
+	movepoint = "Punkt bewegen",
+	swap = "Tauschen",
+	swapmod = "Tausch-Modus aktiviert. Klicke zum Deaktivieren",
+	copy_from_ent = "Kopiere vom Entity",
+	set_pos_self = "Zu deiner Position setzen",
+	set_pos_aim = "Zu anvisiertem Punkt setzen",
+	spawn_point = "Spawnpunkt",
+	spawn_ang = "Spawnwinkel",
+	mark_area = "Markiere Bereich",
+	time_wait = "Zeit zum Warten",
+	map_marker = "Wähle Map-Makierung aus",
+	in_sec = "in Sekunden",
+	def_units = "Standard %s Einheiten", -- "Default 350 units" leave %s as is
+	def_seconds = "Standard %s Sekunden", -- "Default 10 seconds" leave %s as is
+	ent_show_pointer = "Zeiger über dem Entity anzeigen",
+	ent_arcade_style = "Erscheinungsbild des Entitys im Arcade-Stil",
+	ent_stnd_style = "Standard Entity Erscheinungsbild",
+	custom_color = "Aktiviere eigene Farbe",
+	mat_default = "Leer lassen für Standardmaterial",
+
+
+	set_ui = "UI Einstellungen",
+	set_hud = "HUD Einstellungen",
+	set_hud_pos = "Quest HUD Position",
+	set_hud_themes = "HUD Themes",
+	set_server = "Server Einstellungen",
+	set_ui_blur = "Unscharfer Hintergrund",
+	set_ui_mono = "Einfarbiger Hintergrund",
+	set_ui_vignette = "Vignetteneffekt für den Hintergrund",
+	set_ui_brightness = "Helligkeit des Hintergrunds",
+	set_ui_color = "Wähle die Hauptfarbe",
+	set_ui_align_left = "Horizontale Ausrichtung nach links",
+	set_ui_align_right = "Horizontale Ausrichtung nach rechts",
+	set_ui_align_top = "Vertikale Ausrichtung nach oben",
+	set_ui_align_bottom = "Vertikale Ausrichtung nach unten",
+	set_ui_offset_h = "Horizontaler Versatz",
+	set_ui_offset_v = "Vertikaler Versatz",
+
+
+	upl_changes = "Änderungen auf den Server hochladen",
+	res_changes = "Änderungen wiederherstellen",
+
+
+	-- Player
+
+
+	dead = "Du bist tot",
+	time_ex = "Zeit abgelaufen",
+	vehicle_bum = "Dein Fahrzeug wurde zerstört",
+	left_area = "Du hast den Bereich verlassen",
+	m_blew = "Du hast die Mission gesprengt",
+	m_failed = "Mission fehlgeschlagen",
+	m_success = "Mission erfolgreich",
+	m_loop = "Mission aktualisieren",
+
+	-- Errors
+
+
+	inv_quest = "Ungültige Quest",
+	team_bl = "Dein Team ist auf der Blacklist",
+	no_players = "Der Server benötigt mehr Spieler, die gleichzeitig online sind, bevor du fortfahren kannst.",
+	no_players_team = "Der Server benötigt mehr Spieler, die gleichzeitig in spezifischen Teams online sind, bevor du fortfahren kannst.",
+	need_admin = "Nur Admins können diese Aktion durchführen",
+
+
+	-- Quests
+
+
+	active_quest = "Du hast eine aktive Quest",
+	inactive_quest = "Du kannst diese Quest nicht spielen",
+	quest_editor = "Quest Editor",
+	quest_list = "Quest Liste",
+	quests = "Quests",
+	leave_pnt = "Punkt zum Verlassen",
+
+
+	q_editobj = "Objekte bearbeiten",
+	q_incvobj = "Ungültige Objekte",
+	q_setobj = "Objekt Einstellungen",
+	q_newobj = "Neues Objekt hinzufügen",
+	q_editrwd = "Belohnungen bearbeiten",
+	q_rwdeditor = "Belohnungs Editor",
+	q_rwdlist = "Belohnungsliste",
+	q_rwdsets = "Belohnungseinstellungen",
+	q_findmap = "Quests aus anderen Maps suchen",
+	q_obj_des = "Objekt Beschreibung",
+	q_dist_point = "Distanz zum Punkt",
+	q_dist_from_point = "Distanz vom Punkt",
+	q_ignore_veh = "Quest Fahrzeug ignorieren",
+	q_timer_show = "Dem Spieler einen Timer anzeigen",
+	q_area_stay = "Der Spieler muss im Bereich bleiben",
+	q_start = "Quest starten",
+	q_new = "Neue Quest",
+	q_submit = "Quest abschicken",
+	q_addnew = "Neue Quest hinzufügen",
+	q_remove = "Quest entfernen",
+	q_id_unique = "Die ID muss für jede Quest einzigartig sein",
+	q_complete_msg = "Nachricht für fertiggestellte Quest",
+	q_dotime = "Nachricht für fertiggestellte Quest",
+	q_dotime_ok = "Schließe die Quest am Ende der Zeit ab",
+	q_dotime_fail = "Scheitere an der Quest am Ende der Zeit",
+	q_death_fail = "Scheitere die Quest beim Tod des Spielers",
+	q_loop = "Schleifenaufgaben Quest",
+	q_loop_reward = "Belohne Spieler bei jeder Wiederholung",
+	q_enable = "Aktiviere Quest",
+	q_events = "Events",
+	q_eventadd = "Event hinzufügen",
+	q_eventedit = "Event bearbeiten",
+	q_eventremove = "Event entfernen",
+	q_in_progress = "Quest in Bearbeitung",
+	q_time_left = "Verbleibende Zeit",
+	q_ply_limit = "Spielerlimit für die Quest",
+	q_ply_team_limit = "Teamlimit einrichten",
+	q_ply_team_need = "Benötigte Spieler im Team",
+	q_ply_need = "Zum Starten benötigte Anzahl der Spieler",
+	q_play_limit = "Es gibt ein Limit wie viele Spieler diese Quest spielen können",
+	q_must_stay_area = "Du musst in diesem Bereich bleiben, oder die Quest wird fehlgeschlagen",
+	q_time_wait = "Du musst warten bevor du die Quest wiederholen kannst",
+	q_dotime_reset = "Zeit für die Quest zurücksetzen",
+	q_dotime_add = "Zeit zur Quest hinzufügen",
+	q_noreplay = "Du kannst diese Quest nicht wiederholen",
+	q_dis_replay = "Wiederholung der Quest deaktivieren",
+	q_needquest = "Du musst zuerst eine andere Quest abschließen",
+	q_needquest_menu = "Benötigt abgeschlossene Quest",
+	q_enterror = "Quest Entities sind nicht gespawnt, überprüfe die Einrichtung der Quest",
+	q_get = "Du kannst eine Quest von diesen NPCs erhalten",
+	q_noquests = "Es gibt noch keine Möglichkeit Quests zu spielen :(",
+	q_ent_draw = "Quest Entity Drawdistanz",
+	q_loop_stop_key = "Schleifenquest Stoptaste",
+	q_hold_key_stop = "Um die Quest zu stoppen, halte [%s]", -- To stop quest hold [P]
+	q_enter_veh = "Steig in dein Fahrzeug ein",
+	q_npc_link = "Verlinke Quest zu einem NPC",
+	q_icon68 = "Gib eine URL zu einem .PNG Bild mit 68x68px ein",
+	q_ent_pos_show = "Zeige dem Spieler die Positionen der Entities",
+	q_area_size = "Größe des Bereichs",
+	q_area_pos = "Position des Bereichs",
+	q_s_area_size = "Suche Größe des Bereichs",
+	q_s_area_pos = "Suche Position des Bereichs",
+	q_npc_answer_ok = "Positive Antwort des Spielers",
+	q_npc_answer_no = "Negative Antwort des Spielers",
+	q_npc_answer_noq = "Antwort des Spielers bei keiner Quest",
+	q_npc_quest_no = "NPC Sprache bei keiner Quest",
+	q_money_give = "Geld zum Geben",
+
+
+	-- Simple NPCs
+
+
+	npc_editor = "NPC Editor",
+	npc_new = "Neuer NPC",
+	npc_select = "Wähle einen NPC",
+	npc_e_speech = "Gib NPC Sprache ein",
+	npc_submit = "Bestätige NPC Erstellung",
+	npc_update = "Aktualisiere NPC",
+	npc_remove = "Entferne NPC",
+	npc_q_enable = "Aktiviere Quest NPCs",
+	npc_did_open = "Dialog ID zum Öffnen",
+	npc_q_target = "NPC ist ein objektives Ziel",
+	npc_hostile = "Feindlicher NPC",
+
+
+}
+
+
+-- Other phrases
+local lng = "de"
+
+
+MSD.Language[lng]["Move to point"] = "Zum Punkt bewegen"
+MSD.Language[lng]["Leave area"] = "Verlasse Bereich"
+MSD.Language[lng]["Kill NPC"] = "Töte NPC"
+MSD.Language[lng]["Collect quest ents"] = "Sammle Quest Gegenstände"
+MSD.Language[lng]["Talk to NPC"] = "Spreche mit dem NPC"
+
+
+MSD.Language[lng]["There is no quests avalible"] = "Es sind keine Quests verfügbar"
+
+
+MSD.Language[lng]["Give weapon"] = "Gebe Waffe"
+MSD.Language[lng]["Give ammo"] = "Gebe Munition"
+MSD.Language[lng]["Strip Weapon"] = "Nehme Waffe weg"
+MSD.Language[lng]["Spawn quest entity"] = "Spawne Quest Gegenstand"
+MSD.Language[lng]["Spawn entity"] = "Spawne Gegenstand"
+MSD.Language[lng]["Spawn npc"] = "Spawne NPC"
+MSD.Language[lng]["Manage do time"] = "Verwalte Do-Zeit"
+MSD.Language[lng]["Spawn vehicle"] = "Spawne Fahrzeug"
+MSD.Language[lng]["Remove vehicle"] = "Entferne Fahrzeug"
+MSD.Language[lng]["Remove all entites"] = "Entferne alle Gegenstände"
+MSD.Language[lng]["Set HP"] = "Setze HP"
+MSD.Language[lng]["Set Armor"] = "Setze Rüstung"
+
+
+MSD.Language[lng]["DarkRP Money"] = "DarkRP Geld"
+
+
+MSD.Language[lng]["Quest NPCs are disabled"] = "Quest NPCs sind deaktiviert"
+MSD.Language[lng]["You can enable them in settings"] = "Du kannst sie in den Einstellungen aktivieren"
+MSD.Language[lng]["Wait time"] = "Wartezeit"
---
+++ lua/msd/language/tr.lua
@@ -0,0 +1,269 @@
+MSD.Language["tr"] = {
+
+	-- UI
+
+	lang_name = "Türkçe",
+
+	ok = "OK",
+	map = "Harita",
+	off = "Kapalı",
+	on = "Açık",
+	time_add = "Eklenecek zaman",
+	type = "Tür",
+	delay = "Gecikme",
+	cancel = "İptal",
+	enable = "Aktif Et",
+	model = "Model",
+	name = "İsim",
+	settings = "Ayarlar",
+	editor = "Düzenleyici",
+	red = "Kırmızı",
+	green = "Yeşil",
+	blue = "Mavi",
+	admin_menu = "Yetkili Menüsü",
+	ui_settings = "Arayüz Ayarları",
+	active = "Aktif",
+	inactive = "Deaktif",
+	disabled = "Devre Dışı",
+	warning = "Uyarı!",
+	remove = "Kaldır",
+	theme = "Tema",
+	dark_theme = "Karanlık Tema",
+	payment = "Ödeme",
+	load_autosave = "Son kaydı yükleyecek misin?",
+	load_save = "Kaydı yükle",
+	create_new = "Yeni oluştur",
+	enable_option = "Ayarı aktif et",
+	main_opt = "Ana ayarlar",
+	copy_data = "Veriyi kopyala",
+	save_chng = "Değişiklikleri kaydet",
+	enter_name = "İsimi giriniz",
+	enter_id = "ID giriniz",
+	confirm_action = "Eylemlerinizi lütfen onaylayınız",
+	check_fpr_errors = "Hataları kontrol et",
+	enter_description = "Açıklama gir",
+	cooldown_ok = "Başarı sonucu bekleme süresi",
+	cooldown_fail = "Hata sonucu bekleme süresi",
+	s_team_whitelist = "Takım beyaz listesini ayarla",
+	whitelist_blacklist = "Beyaz liste kara liste",
+	custom_val = "Özel değer ayarla",
+	set_hp_full = "Sağlık değerini fulle",
+	dist_to_close = "En yakınına mesafe",
+
+	e_text = "Yazı giriniz",
+	e_number = "Sayı giriniz",
+	e_class = "Sınıf giriniz",
+	e_value = "Değer giriniz",
+	e_blank_dis = "Devre dışı bırakmak için boş bırakın",
+	e_blank_default = "Varsayılan ayarlar için boş bırakın",
+	e_url = "Bağlantı giriniz",
+	e_model = "Model uzantısını giriniz",
+	e_material = "Materyal uzantısını giriniz",
+	e_wep_class = "Silah sınıfını giriniz",
+	e_ent_class = "Varlık sınıfını giriniz",
+	e_veh_class = "Araç sınıfını giriniz",
+	e_npc_class = "NPC sınıfını giriniz",
+
+	select_ammo = "Seçili mermi",
+	amount_ammo = "Mermi asyısı",
+	disable_phys = "Fiziği devre dışı bırak",
+	none = "Hiç",
+	custom_icon = "Özel ikon ayarla",
+	weapon_name = "Silah isim",
+	moveup = "Yukarı git",
+	movedown = "Aşağıya git",
+	movepoint = "Noktayı hareket ettir",
+	swap = "Değiş",
+	swapmod = "Değişim modu aktif. Tıklayarak devre dışı bırak",
+	copy_from_ent = "Baktığın varlıktan kopyala",
+	set_pos_self = "Pozisyonunu ayarla",
+	set_pos_aim = "Baktığın noktaya ayarla",
+	spawn_point = "Nokta oluştur",
+	spawn_ang = "Açı oluştur",
+	mark_area = "Alanı işaretle",
+	time_wait = "Bekleme süresi",
+	map_marker = "Harita işaretçisini ayarla",
+	in_sec = "saniye olarak",
+	def_units = "Varsayılan %s ünit", -- "Default 350 units" leave %s as is
+	def_seconds = "Varasyılan %s saniye", -- "Default 10 seconds" leave %s as is
+	ent_show_pointer = "İaşretçiyi varlığın üstünde göstr",
+	ent_arcade_style = "Arcade-tarzında entitiy görünüşü",
+	ent_stnd_style = "Standart varlık görünüşü",
+	custom_color = "Özel rengi aktif et",
+	mat_default = "Varsayılan materyal için boş bırak",
+
+	set_ui = "Kullanıcı Arayüzü ayarları",
+	set_hud = "HUD ayarları",
+	set_hud_pos = "Görev HUD'u ayarları",
+	set_hud_themes = "HUD Temaları",
+	set_server = "Sunucu ayarları",
+	set_ui_blur = "Arkaplanı bulanıklaştır",
+	set_ui_mono = "Monokrom arkaplan",
+	set_ui_vignette = "Arka plan için vignette efekti",
+	set_ui_brightness = "Arkaplan parlaklığı",
+	set_ui_color = "Ana rengi seç",
+	set_ui_align_left = "Sola yatay hizalama",
+	set_ui_align_right = "Sağa yatay hizalama",
+	set_ui_align_top = "Yukarıya dikey hizalama",
+	set_ui_align_bottom = "Aşağıya dikey hizalama",
+	set_ui_offset_h = "Yatay Ofset",
+	set_ui_offset_v = "Dikey Ofset",
+
+	upl_changes = "Değişiklikleri sunucuya yükle",
+	res_changes = "Değişimleri eski haline getir",
+
+	-- Player
+
+	dead = "Öldün",
+	time_ex = "Zaman doldu",
+	vehicle_bum = "Aracın yok oldu",
+	left_area = "Bölgeyi terk ettin",
+	m_blew = "Görevi batırdın",
+	m_failed = "Görev başarısız",
+	m_success = "Görev başarılı",
+	m_loop = "Görev güncellemesi",
+
+	-- Errors
+
+	inv_quest = "Geçersiz görev",
+	team_bl = "Takımın karalistede",
+	no_players = "Bunu yapman için sunucuda daha fazla oyuncu olması gerekmekte",
+	no_players_team = "Bunu yapman için belirli takımlarda daha fazla oyuncu olması gerekmekte",
+	need_admin = "Sadece adminler bu eylemi gerçekleştirebilir",
+
+	-- Quests
+
+	active_quest = "Aktif bir görevin var",
+	inactive_quest = "Bu görevi oynayamazsın",
+	quest_editor = "Görev Düzenleyicisi",
+	quest_list = "Görev Listesi",
+	quests = "Görevler",
+	leave_pnt = "Noktadan ayrıl",
+
+	q_editobj = "hedefleri düzenle",
+	q_incvobj = "Geçersiz hedef",
+	q_setobj = "Hedef ayarları",
+	q_newobj = "Yeni bir hedef oluştur",
+	q_editrwd = "Ödülleri düzenle",
+	q_rwdeditor = "Ödül Düzenleyicisir",
+	q_rwdlist = "Ödül Listesi",
+	q_rwdsets = "Ödül Ayarları",
+	q_findmap = "Diğer haritalardan görev bul",
+	q_obj_des = "Hedef açıklaması",
+	q_dist_point = "Noktaya olan uzaklık",
+	q_dist_from_point = "Noktadan olan uzaklık",
+	q_ignore_veh = "Görev aracını görmezden gel",
+	q_timer_show = "Oyuncuya zamanlayıcıyı göster",
+	q_area_stay = "Oyuncu bölgede kalmalı",
+	q_start = "Görevi başlat",
+	q_new = "Yeni görev",
+	q_submit = "Görevi sun",
+	q_addnew = "Yeni görev ekle",
+	q_remove = "Görevi kaldır",
+	q_id_unique = "Her bir görevin ID'si kendine özel olmalı",
+	q_complete_msg = "Görevi bitirme mesajı",
+	q_dotime = "Görev süresi",
+	q_dotime_ok = "Süre bitince görev başarıyla tamamlansın",
+	q_dotime_fail = "Süre bitince görev başarısız olsun",
+	q_death_fail = "Oyuncu öldüğünde görevi iptal et",
+	q_loop = "Görevleri tekrara al",
+	q_loop_reward = "Her bir tekrarda oyuncuyu ödüllendir",
+	q_enable = "Görevi aktif et",
+	q_events = "Etkinlikler",
+	q_eventadd = "Etkinlik Ekle",
+	q_eventedit = "etkinlik düzenle",
+	q_eventremove = "Etkinlik kaldır",
+	q_in_progress = "Görev işlem sürecinde",
+	q_time_left = "Kalan süre",
+	q_ply_limit = "Görev için azami oyuncu sayısı",
+	q_ply_team_limit = "Takım limitlerini ayarla",
+	q_ply_team_need = "Gerekli takım oyuncuları",
+	q_ply_need = "Başlamak için gereken oyuncu sayısı",
+	q_play_limit = "Görevi kaç oyuncunun oynayabileceğinin limiti",
+	q_must_stay_area = "Bu bölgenin içinde kalman gerekiyor yoksa görev başarısız olur",
+	q_time_wait = "Bu görevi tekrarlaman için beklemen lazım",
+	q_dotime_reset = "Görevin süresini sıfırla",
+	q_dotime_add = "Göreve yapma süresi ekle",
+	q_noreplay = "Bu görevi tekrar yapamazsın",
+	q_dis_replay = "Görev tekrarını iptal et",
+	q_needquest = "İlk başka bir görevi yapman gerekmekte",
+	q_needquest_menu = "Bitirilmiş görev gerekmektedir",
+	q_enterror = "Görev varlıkları oluşmamakta, görev kurulumunu kontrol et",
+	q_get = "Bu NPC'lerden görev alabilirsin",
+	q_noquests = "Görev yapabileceğin herhangi bir yol bulunmamakta :(",
+	q_ent_draw = "Görev varlığı çizim mesafesi",
+	q_loop_stop_key = "Görev tekrarlama durdurma tuşu",
+	q_hold_key_stop = "görevi durdurmak için [%s] tuşuna basılı tutun", -- To stop quest hold [P]
+	q_enter_veh = "Aracına bin",
+	q_npc_link = "Görevi bir NPC'ye bağla",
+	q_icon68 = ".PNG ve 68x68 pixel olucak şekilde bir bağlantı giriniz",
+	q_ent_pos_show = "Varlık lokasyonlarını oyunculara göster",
+	q_area_size = "Bölge boyutu",
+	q_area_pos = "Bölge pozisyonu",
+	q_s_area_size = "Bölge boyutunu ara",
+	q_s_area_pos = "Bölge pozisyonunu ara",
+	q_npc_answer_ok = "Oyuncunun pozitif cevabı",
+	q_npc_answer_no = "Oyuncunun negatif cevabı",
+	q_npc_answer_noq = "Görev yoksa oyuncunun cevabı",
+	q_npc_quest_no = "Görev yoksa NPC'nin konuşması",
+	q_money_give = "Verilecek para",
+
+	-- Simple NPCs
+
+	npc_editor = "NPC Düzenleyicisi",
+	npc_new = "Yeni NPC",
+	npc_select = "Bir NPC seçin",
+	npc_e_speech = "NPC konuşması giriniz",
+	npc_submit = "NPC oluşumunu onayla",
+	npc_update = "NPC'yi güncelle",
+	npc_remove = "NPC'yi kaldır",
+	npc_q_enable = "Görev NPC'lerini aktif et",
+	npc_did_open = "Dialog ID'si açılırken gereksin",
+	npc_q_target = "NPC objektif bir hedef",
+	npc_hostile = "Düşman NPC",
+
+
+	-- Update 1.0.1
+
+	duration = "Süre",
+	dis_text = "Sergilenen Yazı",
+	cam_speed = "Kamera hızı (düşük sayı - düşük hız)",
+	fov_speed = "FOV değişme hızı (düşük sayı - düşük hız)",
+	cam_start = "Kamera başlama parametreleri",
+	cam_end = "Kamera parametrelerini bitir",
+	cam_pos = "Kamera pozisyonu",
+	cam_ang = "Kamera açısı",
+	cam_fov = "Kamera FOV'u",
+	cam_effect = "Kamera kepenk efekti",
+	not_spawned = "oluşturulmamış",
+
+}
+
+-- Other phrases
+local lng = "tr"
+
+MSD.Language[lng]["Move to point"] = "Noktaya git"
+MSD.Language[lng]["Leave area"] = "Bölgeden ayrıl"
+MSD.Language[lng]["Kill NPC"] = "NPC'yi öldür"
+MSD.Language[lng]["Collect quest ents"] = "Görev varlıklarını topla"
+MSD.Language[lng]["Talk to NPC"] = "NPC ile konuş"
+
+MSD.Language[lng]["There is no quests available"] = "Mevcut görev bulunmamakta"
+
+MSD.Language[lng]["Give weapon"] = "Silah ver"
+MSD.Language[lng]["Give ammo"] = "Mermi ver"
+MSD.Language[lng]["Strip Weapon"] = "Silahı soy"
+MSD.Language[lng]["Spawn quest entity"] = "Görev varlığı oluştur"
+MSD.Language[lng]["Spawn entity"] = "Varlık oluştur"
+MSD.Language[lng]["Spawn npc"] = "NPC oluştur"
+MSD.Language[lng]["Manage do time"] = "Yapım süresini yönet"
+MSD.Language[lng]["Spawn vehicle"] = "Araç oluştur"
+MSD.Language[lng]["Remove vehicle"] = "Aracı kaldır"
+MSD.Language[lng]["Remove all entites"] = "Tüm varlıkları kaldır"
+MSD.Language[lng]["Set HP"] = "HP Ayarla"
+MSD.Language[lng]["Set Armor"] = "Zırh Ayarla"
+
+MSD.Language[lng]["DarkRP Money"] = "DarkRP Parası"
+
+MSD.Language[lng]["Quest NPCs are disabled"] = "Görev NPC'leri devre dışı"
+MSD.Language[lng]["You can enable them in settings"] = "Ayarlardan aktif edebilirsin"
